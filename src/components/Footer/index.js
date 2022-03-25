c4373cd4ac823a6fe9e8efea90a659353c18f6c9
Alexys Carrasquillo <alexys.carrasquillo@gmail.com>
2022-03-25 11:01:45 -0400

    set up footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,27 @@
+
+function Footer() {
+
+return (
+    <footer>
+        <section>
+            <ul>
+                <li>
+                    <a href="https://www.linkedin.com/in/alexys-carrasquillo-a310a9161/">LinkedIn</a>
+                </li>
+                <li>
+                    <a href="https://github.com/AlexysCa">GitHub</a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/lxy.crsql/">Instagram</a>
+                </li>
+                <li>
+                    <h2>Made by Alexys &copy; 2022.</h2>
+                </li>
+            </ul>
+        </section>
+    </footer>
+)
+
+}
+
+export default Footer;
